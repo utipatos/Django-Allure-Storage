517bf8890e2a4599fddfb34241a9c2753fb51ca1
vkornieiev <viktor.kornieiev@gmail.com>
2023-04-09 12:44:33 +0300

    added DB cascade relation

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AllureResult',
             fields=[
-                ('id', models.UUIDField(primary_key=True, serialize=False)),
-                ('path', models.CharField(max_length=100)),
+                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('path', models.FileField(upload_to='')),
                 ('created_date', models.DateTimeField(auto_now_add=True)),
                 ('updated_date', models.DateTimeField(auto_now=True)),
             ],
@@ -26,7 +26,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('service_name', models.CharField(max_length=100)),
-                ('result_id', models.CharField(max_length=100)),
+                ('result', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='app.allureresult',
+                                                related_name="result_info")),
                 ('env', models.CharField(max_length=20)),
                 ('path', models.CharField(max_length=100)),
                 ('created_date', models.DateTimeField(auto_now_add=True)),
